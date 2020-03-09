cc2fcc24747c6ea9d85706cd66366e4418394b27
ys-0-sy <yuta.saito0703@gmail.com>
2020-03-09 11:13:41 +0900

    普通のdiffを作成

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import { Grid } from '@material-ui/core'
 import { WordContainer } from './components/WordContainer';
 import { TextFieldComponent } from './components/TextFieldComponent';
 
+
+const leftLorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n\nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n"
+const rightLorem = "Lorem Ipsum is samply dammy text of the printing and typosetting industry.\n Lorem Ipsam has bean the industri's standard dummy txt evr sieuce tha 1700s, we hn an unknon printer took a galley of type and scrambled it to make a type specimen book."
+
 function App() {
-  const lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  const [leftTextBox, setLeftTextBox] = useState(lorem)
-  const [rightTextBox, setRightTextBox] = useState(lorem)
+  const [leftTextBox, setLeftTextBox] = useState(leftLorem)
+  const [rightTextBox, setRightTextBox] = useState(rightLorem)
   return (
       <div className="App">
       <header className="App-header">
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Grid container justify="space-evenly" spacing={2}>
+            <Grid container justify="space-evenly" spacing={1}>
               <TextFieldComponent
                 TextBox={
                   {
@@ -31,9 +34,9 @@ function App() {
                   }}
               />
             </Grid>
-            <Grid container justify="space-evenly" spacing={2}>
+            <Grid container justify="space-evenly" spacing={1}>
               <WordContainer texts={[leftTextBox, rightTextBox]} type="added" />
-              <WordContainer texts={[leftTextBox, rightTextBox]} type="added" />
+              <WordContainer texts={[leftTextBox, rightTextBox]} type="removed" />
             </Grid>
           </Grid>
         </Grid>
--- src/components/WordContainer.tsx
+++ src/components/WordContainer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Grid } from '@material-ui/core'
 import * as JsDiff from 'diff'
 import _ from 'lodash'
+import { makeStyles } from '@material-ui/styles'
 
 type Props = {
   texts: string[],
@@ -9,38 +10,65 @@ type Props = {
   type: "added" | "removed",
 }
 
-
-const parseText = (wordsArray: JsDiff.Change[]): JsDiff.Change[] => {
-
-  return wordsArray.map((item) => {
-    const values = item.value.match(/[\s\S]{1,5}/g) || []
-    return values.map((valuesItem, valuesIndex) => {
-       return {
-         count: valuesItem.length,
-         value: valuesItem,
-         hasSpace: valuesItem[0] === ' ' || String(values[valuesIndex - 1]).slice(-1) === ' ',
-        added: item.added,
-        removed: item.removed
-      }
-    })
-  })
-    .reduce((pre, current) => { pre.push(...current); return pre }, [])
+type parseText = {
+  id: string,
+  count: number,
+  value: string,
+  added: boolean,
+  removed: boolean
 }
 
-const numBlank = (item:JsDiff.Change) => {
+const parseText = (wordsArray: JsDiff.Change[]): parseText[] => {
+  return wordsArray.map((item, itemIndex) => {
+    const values = item.value.match(/[\s\S]{1,1}/g) || []
+    return values.map((valuesItem, valuesIndex) => {
+      return {
+        id: `${itemIndex}-${valuesIndex}`,
+        count: valuesItem.length,
+        value: valuesItem,
+        added: item.added || false,
+        removed: item.removed || false
+      }
+    })
+  }).reduce((pre, current) => { pre.push(...current); return pre }, [])
+}
+
+const numReturn = (item:JsDiff.Change) => {
   return (item.value.match(/\n/g) || []).length
 }
 
+const numSpace = (item:JsDiff.Change) => {
+  return (item.value.match(/ /g) || []).length
+}
+
+
+
 export const WordContainer: React.FC<Props> = props => {
+  const classes = useStyles()
+  const computeStyle = (item: parseText) => {
+    return (item.added ? classes.added
+            : item.removed ? classes.removed
+            : classes.text)
+  }
   const diffText = parseText(JsDiff.diffChars(props.texts[0], props.texts[1]).filter((words) => {
+    if (props.type === "added") {
       return !words.added
-    }))
-  console.log(diffText)
+    } else {
+      return !words.removed
+      }
+
+  }))
   return (
-    <Grid item xs={3} style={{textAlign: 'left'}}>
-      {diffText.map(item => {
-        return (<span>{item.value}
-          {_.range(numBlank(item)).map(() => (<><span>[←]</span><br /></>))}
+    <Grid item xs={4} style={{textAlign: 'left', fontSize: 16}}>
+      {diffText.map((item: parseText) => {
+        return (
+          <span
+            key={item.id}
+            className={computeStyle(item)}
+          >
+          {item.value}
+          {_.range(numSpace(item)).map(() => (<><span>&nbsp;</span></>))}
+          {_.range(numReturn(item)).map(() => (<><span>[←]</span><br /></>))}
         </span>)
         })
       }
@@ -48,3 +76,17 @@ export const WordContainer: React.FC<Props> = props => {
   )
 }
 
+
+const useStyles = makeStyles({
+  added: {
+    background: 'green',
+    color: 'white'
+  },
+  removed: {
+    background: 'red',
+    color: 'white'
+  },
+  text: {
+    color: 'black'
+  }
+})
